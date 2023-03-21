00128d778a83bf5f3ba49d3752694babe5b4291d
artich22 <125459645+artich22@users.noreply.github.com>
2023-03-21 11:01:59 +0100

    Update Ejercicio01.java

--- Ejercicio01.java
+++ Ejercicio01.java
@@ -22,7 +22,7 @@ public class Ejercicio01 {
 		}
 		if (ordenado)
 			System.out.println ("la serie esta ordenada");
-			System.out.println ("Commit 03");
+			System.out.println ("Commit 04");
 		else 
 			System.out.println ("la serie no esta ordenada");
 		
